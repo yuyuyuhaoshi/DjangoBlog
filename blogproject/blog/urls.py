b033804907b6f7cf7ba10e1eb30e78868831801f
fish-fisher <tz1031583046@163.com>
2017-04-14 16:58:38 +0800

    2017/4/12

--- blogproject/blog/urls.py
+++ blogproject/blog/urls.py
@@ -3,5 +3,5 @@ from . import views
 
 urlpatterns = [
     url(r'^$', views.index, name='index'),
-    url(r'^post/(?p<pk>[0-9]+)/$',views.detail, name='detail'),
+    url(r'^post/(?p<pk>[0-9]+)/$', views.detail, name='detail'),
 ]
